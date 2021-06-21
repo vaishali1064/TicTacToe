d406edde26da1a2f81f96a4e760bcf2562a0fcb1
vaishali1064 <patelvaishali164@gmail.com>
2021-06-21 22:10:09 +0530

    Added: Ability to choose X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,7 +1,10 @@
 package com.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame 
 {
+	//UC1
 	//Array Declaration
 		public static char [] board = new char [10];
 
@@ -14,13 +17,28 @@ public class TicTacToeGame
 				board[i] = ' '; //Assigning empty space
 			}
 		}
+		//UC2
+		public static char player = ' ';
+		public static char computer = ' ';
+		
+		public static char chooseInput(Scanner scan) {
+			System.out.println(" Enter the input value you want : ");
+			return scan.next().toUpperCase().charAt(0);
+
+		}
 		
 		public static void main(String[] args) 
-		{
+		{	//UC1
 			TicTacToeGame obj = new TicTacToeGame();
 			//calling method using object
 			obj.createBoard();
 			System.out.println(" Board created");
+			
+			//UC2
+			Scanner scan = new Scanner(System.in);
+			char player = chooseInput(scan);
+			char computer = (player == 'X') ? 'O' : 'X';
+			System.out.println(player + " is your choosen value " );
 
 		}
 }
